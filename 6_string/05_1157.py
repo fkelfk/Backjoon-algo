91df036733f5dc40e5bcfd49520d328a5667b901
atesi <96477657+fkelfk@users.noreply.github.com>
2022-06-05 07:20:57 +0900

    string

--- 6_string/05_1157.py
+++ 6_string/05_1157.py
@@ -1,1 +1,16 @@
 #1157
+import sys
+
+# word = sys.stdin.readline().upper().rstrip()
+# most_alphabet = []
+
+
+# for i in word:
+#     num = 0
+#     for j in word:
+#         if i == j:
+#             num += 1
+#     most_alphabet.append(num)
+# print(most_alphabet)
+# print(most_alphabet.index(max(most_alphabet)))
+
---
+++ 6_string/06_1152.py
@@ -0,0 +1,13 @@
+import sys
+
+# sentence = sys.stdin.readline()
+
+# list = []
+# for i in sentence:
+#     list.append(i)
+
+# print(list.count(" ")+1)
+
+sentence = sys.stdin.readline().split()
+
+print(len(sentence))
