d8031b99f459d9d037257428d16b265dc30a74fb
atesi <96477657+fkelfk@users.noreply.github.com>
2022-06-07 08:21:04 +0900

    string

---
+++ 6_string/07_2908.py
@@ -0,0 +1,16 @@
+#2908
+
+from audioop import reverse
+import sys
+
+a, b = map(str,sys.stdin.readline().split())
+reverse_a = ''
+reverse_b = ''
+for i in reversed(str(a)):
+    reverse_a += i
+for i in reversed(str(b)):
+    reverse_b += i
+
+print(max(reverse_a,  reverse_b)) 
+
+#print(''.join(reversed(str(a))))
---
+++ 6_string/08_5622.py
@@ -0,0 +1,15 @@
+#5622
+
+import sys
+
+alphabet = ['ABC', 'DEF', 'GHI', 'JKL', 'MNO', 'PQRS', 'TUV', 'WXYZ']
+
+word = sys.stdin.readline()
+time = 0
+
+for i in alphabet:
+    for j in i:
+        for k in word:
+            if j == k:
+                time += alphabet.index(i)+3
+print(time)
